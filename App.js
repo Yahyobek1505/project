0ae7d27286ba95ab9573a3c9b5b47d06edab9ce3
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2025-04-25 14:40:23 +0500

    Lohiyani boshladim

---
+++ App.js
@@ -0,0 +1,29 @@
+/*let ism = document.getElementById("ism");
+console.log(ism);
+let yosh = document.getElementsByClassName('yosh');
+console.log(yosh);
+
+let kun = document.getElementsByTagName("a")
+console.log(kun);
+
+let yoshi = prompt("Tug'ilgan yil kirit")
+
+ism.textContent = `Ismi: Abdulla`
+yosh[3].textContent = `Siz ${2025 - yoshi}ga kirdingiz`
+*/
+
+
+let yosh = document.querySelector('.yosh')
+let yoshi = document.querySelectorAll('.yosh')
+
+console.log(yosh);
+console.log(yoshi);
+
+/*
+foydalanuvchidan login parol so'rang, agar login parol to'g'ri bo'lsa Sahifamizga hush kelibsiz.
+
+prompt orqali ism so'rang, familya so'rang, tug'ilgan yili so'rang.
+barchasini html sahifada chiqarib bering.
+
+
+*/
